ca898bba4f2807f7bff9167557e8eef174557657
rizwan-poncho <rizwan.sp21@gmail.com>
2024-08-17 17:51:18 +0600

    Header updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,15 @@
 export default {
   content: [
       "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}"
+  "./src/**/*.{js,ts,jsx,tsx}",
+   'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
 
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
